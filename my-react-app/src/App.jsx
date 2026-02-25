0017877e6c50572cbcd7aae7c69473fa9af0eaf7
Katlego Twala <twalakatlego44@gmail.com>
2026-02-25 16:26:54 +0200

    feat: add main layout with Sidebar, Header, and MainContent

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,9 +1,17 @@
-export default function App() {
+import Sidebar from "./components/layout/Sidebar";
+import Header from "./components/layout/Header";
+import MainContent from "./components/layout/MainContent";
+
+function App() {
   return (
-    <div className="flex items-center justify-center h-screen">
-      <h1 className="text-5xl font-bold text-blue-600">
-        Tailwind is working 🎉
-      </h1>
+    <div className="flex h-screen bg-[#f5f56fa]">
+      <Sidebar />
+      <div className="flex-1 flex flex-col">
+        <Header />
+       <MainContent /> 
+      </div>
     </div>
-  )
+  );
 }
+
+export default App;

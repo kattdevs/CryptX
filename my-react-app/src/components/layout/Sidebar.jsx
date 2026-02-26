cbbd22b9f5d5fe573497aff3f246ebc4f3c5c76d
Katlego Twala <twalakatlego44@gmail.com>
2026-02-26 14:19:33 +0200

    fixes overall layout imrpoving structure

--- my-react-app/src/components/layout/Sidebar.jsx
+++ my-react-app/src/components/layout/Sidebar.jsx
@@ -1,16 +1,16 @@
 const Sidebar = () => {
     return(
-        <aside className="w-64 bg-white shadow-sm p-6 hidden md:block">
+        <aside className="hidden lg:flex lg:flex-col w-64 bg-white shadow-sm p-6 ">
             <h1 className="text-xl font-bold mb-10 text-indigo-600">CryptX</h1>
 
-            <nav className="space-y-6 text-gray-500">
+            <nav className="space-y-6 text-gray-500 text-sm">
                 <p className="text-indigo-600 font-semibold">Overview</p>
-                <p>Chart</p>
-                <p>Transactions</p>
-                <p>Wallet</p>
-                <p>Mail Box</p>
-                <p>Setting</p>
-                <p>Logout</p>
+                <p className="hover:text-indigo-600 cursor-pointer">Chart</p>
+                <p className="hover:text-indigo-600 cursor-pointer">Transactions</p>
+                <p className="hover:text-indigo-600 cursor-pointer">Wallet</p>
+                <p className="hover:text-indigo-600 cursor-pointer">Mail Box</p>
+                <p className="hover:text-indigo-600 cursor-pointer">Setting</p>
+                <p className="hover:text-indigo-600 cursor-pointer">Logout</p>
             </nav>
         </aside>
     );

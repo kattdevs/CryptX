6f0d62e63888342fdb89809490f05998785e4cb5
Katlego Twala <twalakatlego44@gmail.com>
2026-02-26 12:02:58 +0200

    added transaction data

---
+++ my-react-app/src/data/transactionData.js
@@ -0,0 +1,8 @@
+const transactions = [
+    {id: 1, name: "Ethereum", amount: "$24,102", time: "Today, 19:30"},
+    {id: 1, name: "Bitcoin", amount: "$4,157", time: "Today, 14:32"},
+    {id: 1, name: "Bitcoin", amount: "$64,784", time: "Today, 13:50"},
+    {id: 1, name: "Litecoin", amount: "$14,265", time: "Today, 09:38"},
+];
+
+export default transactions;
